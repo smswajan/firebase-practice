f1eb2a9e7db62d5ac815ae8eb3873781da590698
Shahjahan Md Swajan <swajan.mis13.du@gmail.com>
2020-09-02 16:50:31 +0600

    Reactjs with firestore and image upload project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { app } from "./base";
+import { useState } from "react";
+import { useEffect } from "react";
+
+const db = app.firestore();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [fileUrl, setFileUrl] = useState(null);
+    const [users, setUsers] = useState([]);
+
+    // OnFileChange
+    const onFileChange = async (e) => {
+        const file = e.target.files[0];
+        const storageRef = app.storage().ref();
+        const fileRef = storageRef.child(file.name);
+
+        await fileRef.put(file);
+        setFileUrl(await fileRef.getDownloadURL());
+    };
+    const onSubmit = (e) => {
+        e.preventDefault();
+        const username = e.target.username.value;
+        if (!username) {
+            return;
+        }
+        db.collection("users").doc(username).set({
+            name: username,
+            avatar: fileUrl,
+        });
+    };
+    useEffect(() => {
+        const fetchUsers = async () => {
+            const usersCollection = await db.collection("users").get();
+            setUsers(
+                usersCollection.docs.map((doc) => {
+                    return doc.data();
+                })
+            );
+        };
+        fetchUsers();
+    }, []);
+    return (
+        <>
+            <div className="jumbotron">
+                <h2 className="text-center mb-5">Practice React</h2>
+                {/* Form Here */}
+                <form onSubmit={onSubmit}>
+                    <input
+                        className=" mb-5"
+                        type="file"
+                        onChange={onFileChange}
+                    />
+                    <input
+                        className="form-control"
+                        type="text"
+                        name="username"
+                        id="username"
+                        placeholder="Name"
+                    />
+                    <button className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+            <div className="container">
+                <ul>
+                    {users.map((user) => {
+                        console.log(user);
+                        return (
+                            <li key={user.name}>
+                                <img
+                                    height="160px"
+                                    src={user.avatar}
+                                    alt={user.name}
+                                />
+                                <p>{user.name} </p>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/base.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+import "firebase/storage";
+
+export const app = firebase.initializeApp({
+    apiKey: "AIzaSyDu-CpU9EzDkfpyfvJn7rYUUfXHTguIaoY",
+    authDomain: "practice-react-c4052.firebaseapp.com",
+    databaseURL: "https://practice-react-c4052.firebaseio.com",
+    projectId: "practice-react-c4052",
+    storageBucket: "practice-react-c4052.appspot.com",
+    messagingSenderId: "911991638453",
+    appId: "1:911991638453:web:d4f736b99e5ff4ffd571fc",
+});
